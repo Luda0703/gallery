cf32bfbc5646625b3a4691410f9dfda53a413de0
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-01-25 19:24:48 +0200

    finished commit

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,55 +1,51 @@
 import { Photo } from "./types";
 
-import style from './index.module.scss';
+import style from "./index.module.scss";
 import { useState } from "react";
 import { MainPhoto } from "./MainPhoto";
 import { PreviewGallery } from "./PreviewGallery";
 import { Navigation } from "./Navigation";
 
 interface WebGalleryProps {
-    photos: Photo[],
-
+  photos: Photo[];
 }
 
-export const WebGallery: React.FC<WebGalleryProps> = ({
-    photos,
-}) => {
-    if(!photos.length) {
-        return null
-    }
+export const WebGallery: React.FC<WebGalleryProps> = ({ photos }) => {
+  if (!photos.length) {
+    return null;
+  }
 
-    const [indexActivePhoto, setIndexActivePhoto] = useState(0);
-    const activePhoto = photos[indexActivePhoto];
-    const prevPhoto = photos[indexActivePhoto - 1];
-    const nextPhoto = photos[indexActivePhoto + 1];
+  const [indexActivePhoto, setIndexActivePhoto] = useState(0);
+  const activePhoto = photos[indexActivePhoto];
+  const prevPhoto = photos[indexActivePhoto - 1];
+  const nextPhoto = photos[indexActivePhoto + 1];
 
-    return (
-        <div className={style.webelartGallery}>
-            <div className={style.webelartGalleryContainer}>
-                <MainPhoto
-                prevPhoto={prevPhoto}
-                activePhoto={activePhoto}
-                nextPhoto={nextPhoto}
-                className={style.webelartMainPhoto}
-                />
-                <Navigation
-                className={style.webelartGalleryNavigation}
-                disabledPrev={!prevPhoto}
-                disabledNext={!nextPhoto}
-                onPrevClick={() => {
-                    setIndexActivePhoto(indexActivePhoto - 1)
-                }}
-                onNextClick={() => {
-                    setIndexActivePhoto(indexActivePhoto + 1)
-                }}
-                />
-            </div>
-            <PreviewGallery
-            activePhotoIndex={indexActivePhoto}
-            photos={photos}
-            className={style.webelartGalleryPreviewList}
-
-            />
-        </div>
-    )
-}
+  return (
+    <div className={style.webelartGallery}>
+      <div className={style.webelartGalleryContainer}>
+        <MainPhoto
+          prevPhoto={prevPhoto}
+          activePhoto={activePhoto}
+          nextPhoto={nextPhoto}
+          className={style.webelartMainPhoto}
+        />
+        <Navigation
+          className={style.webelartGalleryNavigation}
+          disabledPrev={!prevPhoto}
+          disabledNext={!nextPhoto}
+          onPrevClick={() => {
+            setIndexActivePhoto(indexActivePhoto - 1);
+          }}
+          onNextClick={() => {
+            setIndexActivePhoto(indexActivePhoto + 1);
+          }}
+        />
+      </div>
+      <PreviewGallery
+        activePhotoIndex={indexActivePhoto}
+        photos={photos}
+        className={style.webelartGalleryPreviewList}
+      />
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
-import './styles/reset.css';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
+import "./styles/reset.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
